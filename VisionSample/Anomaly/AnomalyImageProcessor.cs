17ac4dd017912871b58baf50c2c52e8fd4335d85
thong12889 <thongchai_12889@hotmail.com>
2022-11-14 14:59:30 +0900

    Add project files.

---
+++ VisionSample/Anomaly/AnomalyImageProcessor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using SkiaSharp;
+
+namespace VisionSample
+{
+    public class AnomalyImageProcessor : SkiaSharpImageProcessor<SKBitmap, float>
+    {
+        const int RequiredWidth = 224;
+        const int RequiredHeight = 224;
+
+        protected override SKBitmap OnPreprocessSourceImage(SKBitmap sourceImage)
+            => sourceImage.Resize(new SKImageInfo(RequiredWidth, RequiredHeight), SKFilterQuality.None);
+
+        protected override Tensor<float> OnGetTensorForImage(SKBitmap image)
+        {
+            var bytes = image.GetPixelSpan();
+
+            // For Ultraface, the expected input would be 320 x 240 x 4 (in RGBA format)
+            var expectedInputLength = RequiredWidth * RequiredHeight * 4;
+
+            // For the Tensor, we need 3 channels so 320 x 240 x 3 (in RGB format)
+            var expectedOutputLength = RequiredWidth * RequiredHeight * 3;
+
+            if (bytes.Length != expectedInputLength)
+                throw new Exception($"The parameter {nameof(image)} is an unexpected length. Expected length is {expectedInputLength}");
+
+            // The channelData array is expected to be in RGB order with a mean applied i.e. (pixel - 127.0f) / 128.0f
+            float[] channelData = new float[expectedOutputLength];
+
+            // Extract only the desired channel data (don't want the alpha)
+            var expectedChannelLength = expectedOutputLength / 3;
+            var greenOffset = expectedChannelLength;
+            var blueOffset = expectedChannelLength * 2;
+
+            for (int i = 0, i2 = 0; i < bytes.Length; i += 4, i2++)
+            {
+                var r = Convert.ToSingle(bytes[i]);
+                var g = Convert.ToSingle(bytes[i + 1]);
+                var b = Convert.ToSingle(bytes[i + 2]);
+                channelData[i2] = (r - 127.0f) / 128.0f;
+                channelData[i2 + greenOffset] = (g - 127.0f) / 128.0f;
+                channelData[i2 + blueOffset] = (b - 127.0f) / 128.0f;
+            }
+
+            return new DenseTensor<float>(new Memory<float>(channelData), new[] { 1, 3, RequiredHeight, RequiredWidth });
+        }
+    }
+}
---
+++ VisionSample/Anomaly/AnomalyMain.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.ML.OnnxRuntime.Tensors;
+using Microsoft.ML.OnnxRuntime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp;
+
+namespace VisionSample
+{
+    public class AnomalyMain : VisionSampleBase<AnomalyImageProcessor>
+    {
+        public const string Identifier = "Anomaly";
+        public const string ModelFilename = "cutting.onnx";
+
+        public AnomalyMain()
+            : base(Identifier, ModelFilename) { }
+
+        protected override async Task<ImageProcessingResult> OnProcessImageAsync(byte[] image)
+        {
+            using var sourceImage = await Task.Run(() => ImageProcessor.GetImageFromBytes(image, 800f)).ConfigureAwait(false);
+            using var preprocessedImage = await Task.Run(() => ImageProcessor.PreprocessSourceImage(image)).ConfigureAwait(false);
+            var tensor = await Task.Run(() => ImageProcessor.GetTensorForImage(preprocessedImage)).ConfigureAwait(false);
+            var predictions = await Task.Run(() => GetPredictions(tensor, sourceImage.Width, sourceImage.Height)).ConfigureAwait(false);
+            var outputImage = await Task.Run(() => ImageProcessor.ApplyAnomalyToImage(predictions, sourceImage)).ConfigureAwait(false);
+
+            return new ImageProcessingResult(outputImage);
+        }
+
+        SKBitmap GetPredictions(Tensor<float> input, int sourceImageWidth, int sourceImageHeight)
+        {
+            // Setup inputs and outputs
+            var inputMeta = Session.InputMetadata;
+            var inputName = inputMeta.Keys.ToArray()[0];
+            var inputs = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor(inputName, input) };
+
+            // Run inference
+            using IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results = Session.Run(inputs);
+
+            var resultsArray = results.ToArray();
+            var map_value = resultsArray[0].AsEnumerable<float>().ToArray();
+            var map_dim = resultsArray[0].AsTensor<float>().Dimensions.ToArray();
+            var score_value = resultsArray[1].AsEnumerable<float>().ToArray();
+            var score_dim = resultsArray[1].AsTensor<float>().Dimensions.ToArray();
+
+            var result = GetResultMaskSK(map_value, map_dim, score_value[0]);
+            result = result.Resize(new SKImageInfo(sourceImageWidth, sourceImageHeight), SKFilterQuality.None);
+            return result;
+        }
+
+        SKBitmap GetResultMaskSK(float[] output_value, int[] output_dim, float output2_value)
+        {
+            SKBitmap skBitmap = new SKBitmap(output_dim[2], output_dim[3]);
+            for (int batch = 0; batch < output_dim[0]; batch++)
+            {
+                for (int cls = 0; cls < output_dim[1]; cls++)
+                {
+                    for (int h = 0; h < output_dim[2]; h++)
+                    {
+                        for (int w = 0; w < output_dim[3]; w++)
+                        {
+                            int idx = (batch * output_dim[1] * output_dim[2] * output_dim[3]) + (cls * output_dim[2] * output_dim[3]) + (h * output_dim[3]) + w;
+
+                            if (output_value[idx] < output2_value)
+                            {
+                                skBitmap.SetPixel(w, h, new SKColor(0, 0, 0, 50));
+                            }
+                            else
+                            {
+                                skBitmap.SetPixel(w, h, new SKColor(255, 0, 0, 50));
+                            }
+                        }
+                    }
+                }
+            }
+            return skBitmap;
+        }
+    }
+}
